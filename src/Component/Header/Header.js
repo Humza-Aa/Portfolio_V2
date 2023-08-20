c124a8856fea788bdfa4a531a0760c6b0c1b9322
Humza-Aa <humzaaamir31@gmail.com>
2023-08-20 12:32:15 -0400

    Initial Commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,35 @@
+export default function Header() {
+  const hLinks = [
+    {
+      name: 'Home',
+      path: ''
+    },
+    {
+      name: 'About',
+      path: ''
+    },
+    {
+      name: 'Experience',
+      path: ''
+    },
+    {
+      name: 'Work',
+      path: ''
+    },
+    {
+      name: 'Contact',
+      path: ''
+    },
+  ]
+  
+  return (
+    <>
+      <div>
+        <div>Humza</div>
+        <div>
+          <a href="/">hello</a>
+        </div>
+      </div>
+    </>
+  );
+}

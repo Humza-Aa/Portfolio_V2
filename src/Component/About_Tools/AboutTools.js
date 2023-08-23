a4ece4e6e2889910fb0d4057d7f818bd291b5bed
Humza-Aa <humzaaamir31@gmail.com>
2023-08-23 17:10:52 -0400

    Added: Skillset Icons Updated: Skillset Layout

--- src/Component/About_Tools/AboutTools.js
+++ src/Component/About_Tools/AboutTools.js
@@ -1,19 +1,114 @@
-import styles from "./AboutTools.module.css"
+import styles from "./AboutTools.module.css";
+import ToolsData from "./Tools_data/ToolsData";
+// import Astro from "./Tools_data/Images/Astro.png"
+const images = require.context("./Tools_data/Images", true);
 
 export default function AboutTools() {
-  return(
+  return (
     <>
-    <div className={styles.skillSet}>
+      <div className={styles.skillSet}>
         <div className={styles.skillTitle}>
-          <div>My Skill Set</div>
+          <div>My Skillset</div>
         </div>
         <div className={styles.skillSetDiv}>
-          <div>Frontend</div>
-          <div>BackEnd</div>
-          <div>Tools</div>
+          <div className={styles.frontendDiv}>
+            <div>Frontend</div>
+            <div>
+              {ToolsData.frontendSkills.map((skill, key) => {
+                return (
+                  <>
+                    <div key={key} className={styles.skillDiv}>
+                      <img
+                        className={`${styles.skillImg} ${skill.name}`}
+                        style={skill.sty ? { width: skill.sty } : null}
+                        src={
+                          skill.adjust
+                            ? images(`./${skill.image}.png`)
+                            : skill.image
+                        }
+                        alt={skill.name}
+                      />
+                      <div>{skill.name}</div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.frontendDiv}>
+            <div>Backend</div>
+            <div>
+              {ToolsData.backendSkills.map((skill, key) => {
+                return (
+                  <>
+                    <div key={key} className={styles.skillDiv}>
+                      <img
+                        className={`${styles.skillImg} ${skill.name}`}
+                        style={skill.sty ? { width: skill.sty } : null}
+                        src={
+                          skill.adjust
+                            ? images(`./${skill.image}.png`)
+                            : skill.image
+                        }
+                        alt={skill.name}
+                      />
+                      <div>{skill.name}</div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.frontendDiv}>
+            <div>Cloud Tools</div>
+            <div>
+              {ToolsData.cloudSkills.map((skill, key) => {
+                return (
+                  <>
+                    <div key={key} className={styles.skillDiv}>
+                      <img
+                        className={`${styles.skillImg} ${skill.name}`}
+                        style={skill.sty ? { width: skill.sty } : null}
+                        src={
+                          skill.adjust
+                            ? images(`./${skill.image}.png`)
+                            : skill.image
+                        }
+                        alt={skill.name}
+                      />
+                      <div>{skill.name}</div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.frontendDiv}>
+            <div>Developer Tools</div>
+            <div>
+              {ToolsData.developerToolsSkills.map((skill, key) => {
+                return (
+                  <>
+                    <div key={key} className={styles.skillDiv}>
+                      <img
+                        className={`${styles.skillImg} ${skill.name}`}
+                        style={skill.sty ? { width: skill.sty } : null}
+                        src={
+                          skill.adjust
+                            ? images(`./${skill.image}.png`)
+                            : skill.image
+                        }
+                        alt={skill.name}
+                      />
+                      <div>{skill.name}</div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
         </div>
-
       </div>
     </>
-  )
+  );
 }
--- src/Component/About_Tools/Tools_data/ToolsData.js
+++ src/Component/About_Tools/Tools_data/ToolsData.js
@@ -1,41 +1,146 @@
 const frontendSkills = [
-  { name: "HTML", image: "html.png" },
-  { name: "CSS", image: "css.png" },
-  { name: "JavaScript", image: "javascript.png" },
-  { name: "TypeScript", image: "typescript.png" },
-  { name: "React", image: "react.png" },
-  { name: "Vue", image: "vue.png" },
-  { name: "Bootstrap", image: "bootstrap.png" },
-  { name: "Angular", image: "angular.png" },
-  { name: "Next.js", image: "nextjs.png" },
-  { name: "Sass", image: "sass.png" },
+  {
+    name: "HTML",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  },
+  {
+    name: "TailWindCss",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+  },
+  {
+    name: "JavaScript",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "TypeScript",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+  },
+  {
+    name: "React",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+  {
+    name: "Vue",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+  },
+  {
+    name: "Bootstrap",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+  },
+  {
+    name: "Angular",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+  },
+  {
+    name: "Next.js",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-line.svg",
+  },
+  {
+    name: "Sass",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+  },
 ];
 
 const backendSkills = [
-  { name: "Node.js", image: "nodejs.png" },
-  { name: "Express", image: "express.png" },
-  { name: "MongoDB", image: "mongodb.png" },
-  { name: "Astro", image: "astro.png" },
-  { name: "Python", image: "python.png" },
+  {
+    name: "Node.js",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg",
+  },
+  {
+    name: "Express",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+  },
+  {
+    name: "MongoDB",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  },
+  {
+    name: "Astro",
+    adjust: true,
+    sty: "50px",
+    image: "Astro",
+  },
+  {
+    name: "Python",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+  },
 ];
 
 const cloudSkills = [
-  { name: "AWS", image: "aws.png" },
-  { name: "Kubernetes", image: "kubernetes.png" },
-  { name: "Docker", image: "docker.png" },
-  { name: "Vercel", image: "vercel.png" },
+  {
+    name: "AWS",
+    sty: "50px",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg",
+    source:
+      "Amazon.com Inc., Apache License 2.0 <http://www.apache.org/licenses/LICENSE-2.0>, via Wikimedia Commons",
+  },
+  {
+    name: "Kubernetes",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg",
+  },
+  {
+    name: "Docker",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+  },
+  { name: "Vercel", adjust: true, sty: "50px", image: "Vercel" },
 ];
 
 const developerToolsSkills = [
-  { name: "Git", image: "git.png" },
-  { name: "Figma", image: "figma.png" },
-  { name: "JUnit", image: "junit.png" },
-  { name: "Postman", image: "postman.png" },
-  { name: "Canvas", image: "canvas.png" },
-  { name: "Bash", image: "bash.png" },
+  {
+    name: "Git",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+  },
+  {
+    name: "Figma",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+  },
+  { name: "JUnit", adjust: true, image: "Junit" },
+  { name: "Postman", adjust: true, image: "Postman" },
+  {
+    name: "Canvas",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg",
+  },
+  {
+    name: "Bash",
+    image:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+  },
 ];
 
 const additionalSkills = [
   { name: "MATLAB", image: "matlab.png" },
   // Add more skills here
 ];
+
+module.exports = {
+  frontendSkills,
+  backendSkills,
+  cloudSkills,
+  developerToolsSkills,
+  additionalSkills,
+};
